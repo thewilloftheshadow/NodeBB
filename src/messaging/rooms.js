3e4e54097717109100cdbe29b2b0dde0833c54f3
Shadow <william.shadow@outlook.com>
2021-02-07 22:25:32 -0600

    Add admin check to chat

--- src/messaging/rooms.js
+++ src/messaging/rooms.js
@@ -38,6 +38,7 @@ module.exports = function (Messaging) {
 	}
 
 	Messaging.newRoom = async (uid, toUids) => {
+		if(!privileges.users.isAdmin(uid)) return throw new Error('Only GameMasters can start chats!');
 		const now = Date.now();
 		const roomId = await db.incrObjectField('global', 'nextChatRoomId');
 		const room = {
